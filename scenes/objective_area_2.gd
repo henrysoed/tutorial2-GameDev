885ae5815d30db7a9ee731d8e0653ea3646d5b0f
Henrysoed <henry.soedibjo@gmail.com>
2025-02-21 18:17:55 +0700

    Finalisasi Tutorial 2

--- scenes/objective_area_2.gd
+++ scenes/objective_area_2.gd
@@ -3,3 +3,4 @@ extends Area2D
 func _on_ObjectiveArea_body_entered(body: RigidBody2D):
 	if (body.name == "NewShip"):
 		print("Reached objective Green!")
+		get_tree().change_scene_to_file("res://scenes/EndLevel.tscn")
